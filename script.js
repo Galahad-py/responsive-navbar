752ccac619a883a79c9a60691d5a228a77928304
king-Lemuel <kinglemuelalpha@gmail.com>
2025-01-04 15:37:51 +0100

    added collapsed panel and post hover functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+const postButton = document.getElementById("postButton")
+const dropdownMenu = document.getElementById("dropdownMenu")
+
+postButton.addEventListener("click", () => {
+    if (dropdownMenu.style.display === "flex") {
+        dropdownMenu.style.display = "none"
+    } else {
+        dropdownMenu.style.display = "flex"
+    }
+});
+
+document.addEventListener("click", (event) => {
+    if (!postButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.style.display = "none";
+    }
+});
